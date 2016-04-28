3ee12eae7ac1b477fe5be78606f348bb002c76fc
千锋 <qianfeng@zhangchaowei.local>
2016-04-28 10:46:44 +0800

    新建工程

---
+++ 2016zcw/2016zcw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2016zcw
+//
+//  Created by 千锋 on 16/4/28.
+//  Copyright © 2016年 张朝伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2016zcw/2016zcw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2016zcw
+//
+//  Created by 千锋 on 16/4/28.
+//  Copyright © 2016年 张朝伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
